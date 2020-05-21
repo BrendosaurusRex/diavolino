497fbe76deb2f625f3323e95c9d45030ddb15687
Brendon Barrios <brbarrios@cpp.edu>
2020-05-21 08:17:13 -0700

    cleans up if-else statements into for loops for shorter code

--- EEPROM_7Seg/EEPROM_7Seg.ino
+++ EEPROM_7Seg/EEPROM_7Seg.ino
@@ -26,54 +26,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Count from 0-16 in binary & parse each bit to config the address
-  for(byte x = 0; x < 16; x++) {
-
-    // Parse bits
-    byte a0 = bitRead(x, 0);
-    byte a1 = bitRead(x, 1);
-    byte a2 = bitRead(x, 2);
-    byte a3 = bitRead(x, 3);
+  // Count from 0-16 & parse into bits to config the address
+  for(int x = 0; x < 16; x++) {
+    
+    int a[4]; // Initialize byte array
+    
+    // Parse bits into byte array
+    for(int b = 0; b < 4; b++) {
+      a[b] = bitRead(x, b);       
+    }
 
     // Configure each address since implemented in binary
-    if(a0 == 1) {
-      digitalWrite(Addr[0], HIGH);
+    for(int num = 0; num < 4; num++) {
+      if(a[num] == 1) {
+        digitalWrite(Addr[num], HIGH);
+      }
+      else {
+        digitalWrite(Addr[num], LOW);
+      }
     }
-    else {
-      digitalWrite(Addr[0], LOW);
-    }
-    
-    if(a1 == 1) {
-      digitalWrite(Addr[1], HIGH);
-    }
-    else {
-      digitalWrite(Addr[1], LOW);
-    }
-    
-    if(a2 == 1) {
-      digitalWrite(Addr[2], HIGH);
-    }
-    else {
-      digitalWrite(Addr[2], LOW);
-    }
-    
-    if(a3 == 1) {
-      digitalWrite(Addr[3], HIGH);
-    }
-    else {
-      digitalWrite(Addr[3], LOW);
-    }
-    
-    digitalWrite(OE, LOW);
+
+    digitalWrite(OE, LOW); // Enable Output
     delay(100);
 
     // Read Data pins and print to Serial Monitor
     for(int i = 7; i >= 0; i--) {
       int pin = digitalRead(Data[i]);
       Serial.print(pin);
-      delay(100);
+      //delay(100); // prints each bit slower
     }
     
+    delay(250); // prints faster
   digitalWrite(OE, HIGH);
   Serial.println();    
   }
