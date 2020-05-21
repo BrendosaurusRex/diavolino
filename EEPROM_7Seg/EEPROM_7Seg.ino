4f9c29fcf6b34ea6d72119b353ffd5d0d1275650
Brendon Barrios <brbarrios@cpp.edu>
2020-05-20 21:19:33 -0700

    adds comments

--- EEPROM_7Seg/EEPROM_7Seg.ino
+++ EEPROM_7Seg/EEPROM_7Seg.ino
@@ -1,3 +1,8 @@
+/*
+* ====================================================================
+* Brendon Barrios | May 20, 2020
+* ====================================================================
+*/
 const char Data[] = {0,13,2,3,4,5,6,7};
 const byte Addr[] = {8,9,10,11};
 #define OE 12
@@ -15,37 +20,43 @@ void setup() {
   digitalWrite(OE, HIGH);
   pinMode(OE, OUTPUT); // Set Output Enable pin as output
 
-//  byte A0=0,A1=0x1,A2=0x2,A3=0x3;
-
   Serial.begin(57600); // Initialize Serial Monitor
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  for(byte x=0; x<16; x++) {
+
+  // Count from 0-16 in binary & parse each bit to config the address
+  for(byte x = 0; x < 16; x++) {
+
+    // Parse bits
     byte a0 = bitRead(x, 0);
+    byte a1 = bitRead(x, 1);
+    byte a2 = bitRead(x, 2);
+    byte a3 = bitRead(x, 3);
+
+    // Configure each address since implemented in binary
     if(a0 == 1) {
       digitalWrite(Addr[0], HIGH);
     }
     else {
       digitalWrite(Addr[0], LOW);
     }
-    byte a1 = bitRead(x, 1);
+    
     if(a1 == 1) {
       digitalWrite(Addr[1], HIGH);
     }
     else {
       digitalWrite(Addr[1], LOW);
     }
-    byte a2 = bitRead(x, 2);
+    
     if(a2 == 1) {
       digitalWrite(Addr[2], HIGH);
     }
     else {
       digitalWrite(Addr[2], LOW);
     }
-    byte a3 = bitRead(x, 3);
+    
     if(a3 == 1) {
       digitalWrite(Addr[3], HIGH);
     }
@@ -56,7 +67,8 @@ void loop() {
     digitalWrite(OE, LOW);
     delay(100);
 
-    for(int i=7; i>=0; i--) {
+    // Read Data pins and print to Serial Monitor
+    for(int i = 7; i >= 0; i--) {
       int pin = digitalRead(Data[i]);
       Serial.print(pin);
       delay(100);
@@ -65,9 +77,4 @@ void loop() {
   digitalWrite(OE, HIGH);
   Serial.println();    
   }
-//  digitalWrite(Addr[0x0], LOW);
-//  digitalWrite(Addr[0x1], HIGH);
-//  digitalWrite(Addr[0x2], LOW);
-//  digitalWrite(Addr[0x3], HIGH);
-    
 }
